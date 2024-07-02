966d1f96552276a27940185ff187ae67132777a9
Jongwoo Choi <muvnelik@naver.com>
2024-07-02 14:24:48 +0900

    [feature] 인증이필요한 요청을 PermitAll로 열어야 접근이 되는 오류 -> 일단 열어놓고 진행

--- src/main/java/com/jonggae/apigateway/security/SecurityConfig.java
+++ src/main/java/com/jonggae/apigateway/security/SecurityConfig.java
@@ -2,38 +2,30 @@ package com.jonggae.apigateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFlux
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/api/customers/login").permitAll()
-                        .anyExchange().authenticated()
-
+                        .pathMatchers("/api/products/**").permitAll()
+                        .anyExchange().permitAll()
                 )
                 .securityContextRepository(NoOpServerSecurityContextRepository.getInstance());
+
         return http.build();
     }
-
 }
