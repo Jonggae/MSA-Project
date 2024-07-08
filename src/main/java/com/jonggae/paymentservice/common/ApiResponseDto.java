893102d5372bc73b0622f3b63b810023988b3cf6
Jongwoo Choi <muvnelik@naver.com>
2024-07-08 09:28:18 +0900

    [feature] 최소 기본 구성 JWT, Security, MessageUtil

---
+++ src/main/java/com/jonggae/paymentservice/common/ApiResponseDto.java
@@ -0,0 +1,42 @@
+package com.jonggae.paymentservice.common;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ApiResponseDto<T> {
+    private boolean success;
+    private String message;
+    private int statusCode;
+    private T data;
+    private String errorCode;
+    private Object errorDetails;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDateTime timeStamp;
+
+    public ApiResponseDto(String message) {
+        this.message = message;
+    }
+
+    public ApiResponseDto(String message, T data) {
+        this.message = message;
+        this.data = data;
+    }
+}
